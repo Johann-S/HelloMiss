6a988de3ab274a0acd61ba92e23e3b453bc14d15
Johann-S <johann.servoire@gmail.com>
2014-01-12 18:37:36 +0100

    Fix pagination on One Day One Babes

--- src/com/jservoire/tools/PaginateHello.java
+++ src/com/jservoire/tools/PaginateHello.java
@@ -94,8 +94,8 @@ public class PaginateHello
 			intWeeks--;
 			
 			String nbWeeks = Integer.toString(intWeeks);
-			if ( intWeeks == 0 ) {
-				nbWeeks = "00";
+			if ( intWeeks < 10 ) {
+				nbWeeks = "0"+nbWeeks;
 			}
 			
 			String numDay = Integer.toString(intNumDay);
@@ -145,10 +145,17 @@ public class PaginateHello
 				
 				cal.add(Calendar.DAY_OF_MONTH, intPage);
 				int intNumDay = this.getNbDay(cal.get(Calendar.DAY_OF_WEEK));
-				int intWeeks = cal.get(Calendar.WEEK_OF_YEAR);
+				
+			    int ordinalDay = cal.get(Calendar.DAY_OF_YEAR);
+			    int weekDay = cal.get(Calendar.DAY_OF_WEEK) - 1;
+			    int intWeeks = (ordinalDay - weekDay + 10) / 7;	
 				intWeeks--;
-
+				
 				String nbWeeks = Integer.toString(intWeeks);
+				if ( intWeeks < 10 ) {
+					nbWeeks = "0"+nbWeeks;
+				}
+
 				String numDay = Integer.toString(intNumDay);
 				String numYear = Integer.toString(cal.get(Calendar.YEAR));
 				pattern = pattern+numYear+"/"+nbWeeks+"/"+numDay+"/";
